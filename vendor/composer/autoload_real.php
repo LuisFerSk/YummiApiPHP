e830634a8dd19f8a607a9d5a97ee2a187072f0b5
LuisFerSk <lcampomontero@gmail.com>
2022-10-11 04:37:11 -0500

    install phpspreadsheet

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,25 @@ class ComposerAutoloaderInit57e19340f6cbf26826939510a1f397c2
 
         $loader->register(true);
 
+        $includeFiles = \Composer\Autoload\ComposerStaticInit57e19340f6cbf26826939510a1f397c2::$files;
+        foreach ($includeFiles as $fileIdentifier => $file) {
+            composerRequire57e19340f6cbf26826939510a1f397c2($fileIdentifier, $file);
+        }
+
         return $loader;
+    }
+}
+
+/**
+ * @param string $fileIdentifier
+ * @param string $file
+ * @return void
+ */
+function composerRequire57e19340f6cbf26826939510a1f397c2($fileIdentifier, $file)
+{
+    if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+        $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+        require $file;
     }
 }
