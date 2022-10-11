e9daf2eb6b7cf5d30bb251090866ba5cc4610454
LuisFerSk <lcampomontero@gmail.com>
2022-10-11 04:40:04 -0500

    add id to access controll headers

--- index.php
+++ index.php
@@ -2,13 +2,13 @@
 
 header('Access-Control-Allow-Origin: *');
 header("Access-Control-Allow-Methods: HEAD, GET, POST, PUT, PATCH, DELETE, OPTIONS");
-header("Access-Control-Allow-Headers: token, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+header("Access-Control-Allow-Headers: id, token, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
 header('Content-Type: application/json');
 
 $method = $_SERVER['REQUEST_METHOD'];
 if ($method == "OPTIONS") {
     header('Access-Control-Allow-Origin: *');
-    header("Access-Control-Allow-Headers: token, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
+    header("Access-Control-Allow-Headers: id, token, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method,Access-Control-Request-Headers, Authorization");
     header("HTTP/1.1 200 OK");
     die();
 }
@@ -29,6 +29,8 @@ include_once 'models/usuario.model.php';
 include_once 'models/equipo.model.php';
 include_once 'models/funcionario.model.php';
 include_once 'models/periferico.model.php';
+
+include_once 'utils/excel.php';
 
 ini_set('display_errors', 1);
 ini_set('log_errors', 1);
