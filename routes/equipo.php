581669c9b6ddcf86c4d9d489454197db0e15f34b
LuisFerSk <lcampomontero@gmail.com>
2022-10-11 06:25:38 -0500

    creation of services to download excel from equipo

--- routes/equipo.php
+++ routes/equipo.php
@@ -107,3 +107,132 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET' && $_SERVER['REQUEST_URI'] == $routeBase
     Response::sendResponse(new Response(401, 'El token es necesario.'));
     return;
 }
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && $_SERVER['REQUEST_URI'] == $routeBase . 'equipo/excel/all') {
+    if (isset($headers['token'])) {
+        $dbController = new DbController(Config::$DB['equipo_table']);
+        $equipoController = new EquipoController($dbController);
+
+        $resultValidarToken = $dbController->validarToken($headers['token']);
+
+        if ($resultValidarToken->status != 200) {
+            Response::sendResponse($resultValidarToken);
+            return $found = true;
+        }
+
+        $response = $equipoController->getAll();
+
+        if ($response->status != 200) {
+            $message = 'No se ha podido generar el Excel de equipos: ' . strtolower($response->message);
+            Response::sendResponse(new Response($response->status, $message, $response->data));
+            return $found = true;
+        }
+
+        if (count($response->data) < 1) {
+            $message = 'No se ha encontrado datos para generar un Excel.';
+            Response::sendResponse(new Response(404, $message));
+            return $found = true;
+        }
+
+        $dataForExcel = Excel::normalizeQueryData($response->data);
+
+        $nameFileExcel = 'equipo';
+
+        try {
+            Excel::generateExcel($nameFileExcel, $dataForExcel);
+        } catch (Exception $exception) {
+            $message = 'No se ha podido generar el Excel de equipos.';
+            Response::sendResponse(new Response(500, $message, $exception));
+            return $found = true;
+        }
+
+        return $found = true;
+    }
+    Response::sendResponse(new Response(401, 'El token es necesario.'));
+    return $found = true;
+}
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && $_SERVER['REQUEST_URI'] == $routeBase . 'equipo/excel/by-sectorial') {
+    if (isset($headers['token'])) {
+        $dbController = new DbController(Config::$DB['equipo_table']);
+        $equipoController = new EquipoController($dbController);
+
+        $resultValidarToken = $dbController->validarToken($headers['token']);
+
+        if ($resultValidarToken->status != 200) {
+            Response::sendResponse($resultValidarToken);
+            return $found = true;
+        }
+
+        $response = $equipoController->getAllBySectorial($headers['id']);
+
+        if ($response->status != 200) {
+            $message = 'No se ha podido generar el Excel de equipos: ' . strtolower($response->message);
+            Response::sendResponse(new Response($response->status, $message, $response->data));
+            return $found = true;
+        }
+
+        if (count($response->data) < 1) {
+            $message = 'No se ha encontrado datos para generar un Excel.';
+            Response::sendResponse(new Response(404, $message));
+            return $found = true;
+        }
+
+        $dataForExcel = Excel::normalizeQueryData($response->data);
+
+        $nameFileExcel = 'equipo';
+
+        try {
+            Excel::generateExcel($nameFileExcel, $dataForExcel);
+        } catch (Exception $exception) {
+            $message = 'No se ha podido generar el Excel de equipos.';
+            Response::sendResponse(new Response(500, $message, $exception));
+            return $found = true;
+        }
+
+        return $found = true;
+    }
+    Response::sendResponse(new Response(401, 'El token es necesario.'));
+    return $found = true;
+}
+if ($_SERVER['REQUEST_METHOD'] == 'GET' && $_SERVER['REQUEST_URI'] == $routeBase . 'equipo/excel/by-subsector') {
+    if (isset($headers['token'])) {
+        $dbController = new DbController(Config::$DB['equipo_table']);
+        $equipoController = new EquipoController($dbController);
+
+        $resultValidarToken = $dbController->validarToken($headers['token']);
+
+        if ($resultValidarToken->status != 200) {
+            Response::sendResponse($resultValidarToken);
+            return $found = true;
+        }
+
+        $response = $equipoController->getAllBySubsector($headers['id']);
+
+        if ($response->status != 200) {
+            $message = 'No se ha podido generar el Excel de equipos: ' . strtolower($response->message);
+            Response::sendResponse(new Response($response->status, $message, $response->data));
+            return $found = true;
+        }
+
+        if (count($response->data) < 1) {
+            $message = 'No se ha encontrado datos para generar un Excel.';
+            Response::sendResponse(new Response(404, $message));
+            return $found = true;
+        }
+
+        $dataForExcel = Excel::normalizeQueryData($response->data);
+
+        $nameFileExcel = 'equipo';
+
+        try {
+            Excel::generateExcel($nameFileExcel, $dataForExcel);
+        } catch (Exception $exception) {
+            $message = 'No se ha podido generar el Excel de equipos.';
+            Response::sendResponse(new Response(500, $message, $exception));
+            return $found = true;
+        }
+
+        return $found = true;
+    }
+    Response::sendResponse(new Response(401, 'El token es necesario.'));
+    return $found = true;
+}
